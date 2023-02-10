f62dc1817e775cfe30192d9c4bd64391a060f07e
Leander Gmeiner <leander.gmeiner@gmail.com>
2023-02-10 14:51:25 +0100

    Simple route mapping
    Simple routes (i.e. to HTML files) are defined by a json object. The key
    is the route and the value is the path to the file.
    A template file must be provided.
    The directory in which the HTML files are must be provided.
    The routes are built with the buildRoutes function in routes.js.
    
    Added several example routes to index.js.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+import fs from "fs/promises"
+
 import express from "express"
+
+import { buildRoutes } from "./routes.js"
 
 let app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World")
-});
+app.use(express.static("public"));
 
-app.listen(3000);
+/* Here are currently only place holders */
+const routes = {
+  dir: "site/",
+  template: "template.html",
+
+  "/": "/startpage/index.html",
+  "/search/": "/search/index.html",
+  "/login/": "/login/index.html",
+  "/register/": "/register/index.html",
+};
+
+await buildRoutes(app, routes);
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
