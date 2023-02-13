f84b6f643cc466d880f626d36493a4ee9cd993b8
Leander Gmeiner <leander.gmeiner@gmail.com>
2023-02-13 17:23:10 +0100

    Appendix 2: Services

---
+++ services/mongodb.js
@@ -0,0 +1,35 @@
+import "mongoose"
+
+
+function getProducts(req, res) {
+}
+
+function getProductById(req, res) {
+}
+
+function addNewProduct(req, res) {
+}
+
+function getUserById() {
+}
+
+function addNewUser() {
+}
+
+function getUserCredentialValidity() {
+
+  // 1. Get user by email from database
+  // 2. Check if password and password in database are the same.
+  // 3. (Optional) Hash the passwords when creating or reading a user
+}
+
+
+export {
+  getProducts,
+  getProductById,
+  addNewProduct,
+
+  getUserById,
+  addNewUser,
+  getUserCredentialValidity
+}
