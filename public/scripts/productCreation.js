744369fcfa8dfca47242966e389b4d08761bf9a9
danielSiegt08 <dani.siegert@t-online.de>
2023-03-05 15:02:47 +0100

    Added Fileupload

--- public/scripts/productCreation.js
+++ public/scripts/productCreation.js
@@ -1,74 +1,93 @@
+//For Page Preview
 function preview_image(event) {
-    var reader =  new FileReader();
-    reader.onload = function(){
-      var output = document.getElementById('uploadedImage');
-      output.src = reader.result;
-      var filename = event.target.files[0].name;
-      console.log(filename);
-    }
-    reader.readAsDataURL(event.target.files[0]);
-    reader.readAsText(event.target.files[0]); 
-    
-  }
+  var reader =  new FileReader();
+  reader.readAsDataURL(event.target.files[0]);
+  var file = null;
+  reader.onload = function(){
+    var output = document.getElementById('uploadedImage');
+    output.src = reader.result;
+    file = event.target.files[0];
+    var filename = event.target.files[0].name;
+    pushFileToServer(file);
+  } 
+}
 
-  function preview_text(){
-    let title = document.getElementById("productTitle").value;
-    document.getElementById("productPreviewTitle").innerText= title;
+function preview_text(){
+let title = document.getElementById("productTitle").value;
+document.getElementById("productPreviewTitle").innerText= title;
 
-    let description = document.getElementById("productDescription").value;
-    document.getElementById("productPreviewDescription").innerText= description;
+let description = document.getElementById("productDescription").value;
+document.getElementById("productPreviewDescription").innerText= description;
 
-    let price = document.getElementById("productPrice").value;
-    price="Price: "+price+"€"
-    document.getElementById("productPreviewPrice").innerText= price;
+let price = document.getElementById("productPrice").value;
+price="Price: "+price+"€"
+document.getElementById("productPreviewPrice").innerText= price;
 
-  }
-    
+}
+
+function pushFileToServer(file){
+console.log("Arrived");
+console.log(file);
+try{
+    var xhttp = new XMLHttpRequest();  
+    var formData = new FormData();
+    formData.append("image", file); 
+    xhttp.open("post", "/api/upload", true);
+    xhttp.send(formData);
+}
+catch{
+    console.log("something didn't work!");
+}
+}
+
 function pushNewProduct()
 {
-  json = generateJSON();
-  try{
-    var xhttp = new XMLHttpRequest();   
-    xhttp.open("POST", "api/products", true);
-    xhttp.setRequestHeader("Content-type","application/json");
-    console.log(typeof(json));
-    xhttp.send(json);
-    console.log("Finished!")
-    }
-    catch{
-        console.log("something didn't work!");
-    }
+json = generateJSON();
+try{
+  var xhttp = new XMLHttpRequest();   
+  xhttp.open("POST", "api/products", true);
+  xhttp.setRequestHeader("Content-type","application/json");
+  xhttp.send(json);
+  console.log("Finished!")
+  }
+  catch{
+      console.log("something didn't work!");
+  }
 }
 
 function generateJSON(){
-  const imageFile = document.getElementById("uploadImage").value;
-  const productTitle = document.getElementById("productTitle").value;
-  const productDescription = document.getElementById("productDescription").value;
-  const productPrice = parseInt(document.getElementById("productPrice").value);
-  const productManufacturer = document.getElementById("productManufacturer").value;
-  const productColor = document.getElementById("productColor").value;
-  const productOS = document.getElementById("productOS").value;
-  let localProductRAM = document.getElementById("productRAM").value;
-  const productRAM= parseInt(localProductRAM.substring(0, localProductRAM.length - 2));
-  const productTypeCPU = document.getElementById("productTypeCPU").value;
-  const productTypeGPU = document.getElementById("productTypeGPU").value;
+const re = new RegExp("(?<=fakepath).*$");
+const imageFile = document.getElementById("uploadImage").value;
+let filename = imageFile.match(re);
+filename = String(filename).substring(1,String(filename).length);
+const filepath = "img/upload/"+filename;
+const productTitle = document.getElementById("productTitle").value;
+const productDescription = document.getElementById("productDescription").value;
+const productPrice = parseInt(document.getElementById("productPrice").value);
+const productManufacturer = document.getElementById("productManufacturer").value;
+const productColor = document.getElementById("productColor").value;
+const productOS = document.getElementById("productOS").value;
+let localProductRAM = document.getElementById("productRAM").value;
+const productRAM= parseInt(localProductRAM.substring(0, localProductRAM.length - 2));
+const productTypeCPU = document.getElementById("productTypeCPU").value;
+const productTypeGPU = document.getElementById("productTypeGPU").value;
 
-  const mockProduct = 
-    {
-        "productName": productTitle,
-        "productDescription": productDescription,
-        "price": productPrice,
-        "image": [imageFile],
-        "productSpecification": {
-            "operatingSystem": productOS,
-            "amountRAM": productRAM,
-            "color": productColor,
-            "typeCPU": productTypeCPU,
-            "manufactorer":productManufacturer,
-            "typeGPU": productTypeGPU
-        }
-    };
-  
-  const jsonReturn = JSON.stringify(mockProduct);
-  return jsonReturn;
+const mockProduct = 
+  {
+      "productName": productTitle,
+      "productDescription": productDescription,
+      "price": productPrice,
+      "image": [filepath],
+      "productSpecification": {
+          "operatingSystem": productOS,
+          "amountRAM": productRAM,
+          "color": productColor,
+          "typeCPU": productTypeCPU,
+          "manufactorer":productManufacturer,
+          "typeGPU": productTypeGPU
+      }
+  };
+
+const jsonReturn = JSON.stringify(mockProduct);
+return jsonReturn;
 }
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 import express from "express"
+//for image Upload
+import expressFileUpload from 'express-fileupload'
 
 import {addNewProduct, getProductById, getProducts} from "../controllers/api_products_controller.js"
 import {
@@ -12,6 +14,7 @@ import {
 
 const apiRouter = express.Router();
 
+apiRouter.use(expressFileUpload());
 // API Routes
 apiRouter.get("/api/products", getProducts);
 apiRouter.get("/api/products/:id", getProductById);
@@ -24,6 +27,19 @@ apiRouter.post("/api/users", addNewUser);
 
 apiRouter.get("/api/users/:id/shoppingCart", getUserShoppingCart);
 apiRouter.put("/api/users/:id/shoppingCart", addItemToUserShoppingCart);
+
+//Image Upload
+apiRouter.post('/api/upload', (req, res) => {
+  // Get the file that was set to our field named "image"
+  const { image } = req.files;
+  // If no image submitted, exit
+  if (!image) return res.sendStatus(400);
+
+  // Move the uploaded image to our upload folder
+  image.mv('public/img/upload/' + image.name);
+  // All good
+  res.sendStatus(200);
+});
 
 // HTML Document Routes
 apiRouter.get("/", (req, res) => {
