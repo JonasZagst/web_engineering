e3b3e6ee06318f0015a1a43bfe0022980f6e75ee
Leander Gmeiner <leander.gmeiner@gmail.com>
2023-03-06 19:36:57 +0100

    Added documentation for the product services

--- services/products_service.js
+++ services/products_service.js
@@ -3,6 +3,30 @@ import DbGetError from "../errors/dbGetError.js";
 import JSONMappingError from "../errors/JSONMappingError.js";
 import DbPostError from "../errors/dbPostError.js";
 
+/** The type for the product specification of a product.
+ * @typedef {Object} ProductSpecification
+ * @property {string} operatingSystem
+ * @property {number} amountRAM
+ * @property {string} color
+ * @property {string} typeCPU
+ * @property {string} typeGPU
+ * @property {string} manufactorer
+ */
+
+/** The type of a product, such that Mongoose can create a MongoDB Document from it.
+ * @typedef {Object} Product
+ * @property {string} productName
+ * @property {string} [productDescription]
+ * @property {number} price
+ * @property {[string]} image A list of paths to images for the product.
+ * @property {ProductSpecification} productSpecification
+ */
+
+/** The ID type for a product
+ * @typedef {string} ProductID */
+
+/** Get a list of all products that exist.
+ * @returns {[Product]} */
 async function getProducts() {
     try {
         return await Product.find({});
@@ -11,6 +35,10 @@ async function getProducts() {
     }
 }
 
+/** Get a list of all products matching the query.
+ * @param {string} searchString The raw search string as entered by the user
+ *
+ * @returns {[Product]} */
 async function getProductsQuery(searchString) {
     try {
         return await Product.find({ $text: { $search: searchString } });
@@ -19,6 +47,10 @@ async function getProductsQuery(searchString) {
     }
 }
 
+/** Get a product by its ID
+ * @param {ProductID} id The id of the product
+ *
+ * @returns {Product|null} The product or null if `id` has no product associated with it. */
 async function getProductById(id) {
     try {
         return await Product.findById(id);
@@ -31,6 +63,10 @@ async function getProductById(id) {
     }
 }
 
+/** Adds a new product to the database
+ * @param {Product} productJSON
+ *
+ * @returns {Object|null} The new Mongoose object for the product */
 async function addNewProduct(productJSON) {
     const productModel = new Product(productJSON);
     const validationError = productModel.validateSync();
